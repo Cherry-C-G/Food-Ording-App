28064b0b9eaca7ca7b005707ef828fa3ace7e026
Cherry-C-G <99839382+Cherry-C-G@users.noreply.github.com>
2023-07-19 13:03:40 -0500

    initial

---
+++ Mango Services.CouponAPI/Migrations/20230713191415_update entities.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango_Services.CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateentities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/Controllers/ShoppingCartAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+    [Route("api/cart")]
+    [ApiController]
+    public class ShoppingCartAPIController : ControllerBase
+    {
+    }
+}
